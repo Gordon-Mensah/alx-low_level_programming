f08f083614a17868e72611cec967788dd526d612
Gordon-Mensah <131736971+Gordon-Mensah@users.noreply.github.com>
2023-07-08 19:18:09 +0000

    1-memcpy.c

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -4,7 +4,7 @@
  * from memory area src to memory area dest
  *@dest:area where bytes are copied to
  *@src:area where bytes are copied from
- *@n:number of bytes to copy
+ *@n:number of bytes to copy 
  *Return: returns a pointer to n
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
