2d72a5fdb7d2e4a279861078a8fa5b0f89a916dc
Gordon-Mensah <131736971+Gordon-Mensah@users.noreply.github.com>
2023-07-08 19:13:42 +0000

    main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,19 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
-#define TRUE  1
-#define FALSE 0
 
-#include <unistd.h>
-#include <stdio.h>
 
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
-int _abs(int);
+int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
-void _puts(char *str);
+void _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
@@ -27,4 +23,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* for MAIN_H */
+#endif /* MAIN_H */
